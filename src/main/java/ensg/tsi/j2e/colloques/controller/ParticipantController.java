29aac2a70dc17008cdbab9213dfdfb84e2345a87
Cirederf1 <fredericye@hotmail.fr>
2024-02-10 18:36:51 +0100

    Add ParticipantController and eventParticipants.html

--- src/main/java/ensg/tsi/j2e/colloques/controller/ParticipantController.java
+++ src/main/java/ensg/tsi/j2e/colloques/controller/ParticipantController.java
@@ -1,13 +1,31 @@
 package ensg.tsi.j2e.colloques.controller;
 
+import ensg.tsi.j2e.colloques.metier.Evenement;
+import ensg.tsi.j2e.colloques.services.EvenementService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ParticipantController {
+
+    private final EvenementService evenementService;
+
+    public ParticipantController(EvenementService evenementService) {
+        this.evenementService = evenementService;
+    }
 
     @PostMapping("/goToRegister")
     public String goToRegister() {
         return "register";
     }
+
+    @PostMapping("/goToEventParticipants")
+    public String goToEventParticipants(@RequestParam("eventId") Long eventId, Model model) {
+        Evenement e = evenementService.findById(eventId).get();
+        model.addAttribute("participants", e.getParticipants());
+        return "eventParticipants";
+    }
+
 }
